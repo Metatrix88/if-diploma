0039c9842442c81e17a5366a2d7694e1e1189dc4
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2024-01-12 23:12:57 +0300

    created dropdown

---
+++ src/assets/icons/AvatarBtn/AvatarBtn.styles.js
@@ -0,0 +1,39 @@
+import { createUseStyles } from 'react-jss';
+import {
+  DESKTOP_SMALL_BREAKPOINT,
+  MOBILE_BREAKPOINT,
+  TABLET_BREAKPOINT,
+} from '../../../constants/styles.constants';
+
+export const nightIconStyles = (theme) => ({
+  root: {
+    fill: theme.palette.icons.main,
+    width: 40,
+    aspectRatio: '1/1',
+  },
+
+  //For desktops:
+  [`@media (max-width: ${DESKTOP_SMALL_BREAKPOINT})`]: {
+    root: {
+      width: 30,
+    },
+  },
+
+  //For tablets:
+  [`@media (max-width: ${TABLET_BREAKPOINT})`]: {
+    root: {
+      width: 28,
+    },
+  },
+
+  // For mobiles:
+  [`@media (max-width: ${MOBILE_BREAKPOINT})`]: {
+    root: {
+      width: 24,
+    },
+  },
+});
+
+export const useNightIconStyles = createUseStyles(nightIconStyles, {
+  name: 'NightIcon',
+});
